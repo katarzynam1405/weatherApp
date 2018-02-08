c10ef890a65f816fab4210ca63e8739e169ffbdb
pTustanowski <przemyslaw.tustanowski@gmail.com>
2018-02-08 21:01:45 +0100

    markers + weather change

--- src/app.js
+++ src/app.js
@@ -3,14 +3,24 @@ import weatherAPI from './API/weatherAPI';
 import renderNews from './renderOutput/renderNews';
 import geolocation from './geolocation/geolocation';
 import renderOutput from './renderOutput/renderOutput';
-import {showOnMap, findCityOnMap} from './API/googleMapAPI';
+import {showOnMap, findCityOnMap, placeMarkerAndPanTo} from './API/googleMapAPI';
 import celciusConverter from './tempConverter/tempConverter';
 
 var GoogleMapsLoader = require('google-maps');
 var map;
+var marker;
 GoogleMapsLoader.load(function(google) {
   var myLatLng = {lat: 52.158742, lng: 18.120850};
   map = new google.maps.Map(document.querySelector('#googleMap'), {center: myLatLng, zoom: 6});
+
+  google.maps.event.addListener(map, "click", function (e) {
+    if(marker !== undefined){
+      marker.setMap(null);
+    }
+    marker = placeMarkerAndPanTo(e.latLng, map);
+
+    weatherAPI(e.latLng.lat(), e.latLng.lng()).then(renderOutput).then(newsAPI().then(renderNews)).catch(e => alert(e.errorMessage));
+  });
 });
 GoogleMapsLoader.KEY = 'AIzaSyDUrcoLs6wrba-vb9YGlEeItYXsJ_8S5FA';
 
@@ -20,7 +30,10 @@ var cityQuery = document.querySelector('#searchCity');
 var output = document.querySelector('.output');
 
 function onGeolocationSuccess(position) {
-    showOnMap(position.coords.latitude, position.coords.longitude, 13, map);
+    if(marker !== undefined){
+      marker.setMap(null);
+    }
+    marker = showOnMap(position.coords.latitude, position.coords.longitude, 13, map);
     weatherAPI(position.coords.latitude, position.coords.longitude).then(renderOutput).then(newsAPI().then(renderNews)).catch(e => alert(e.errorMessage));
 }
 
